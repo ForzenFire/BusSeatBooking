3e75a2ccd304ff7b13cc0500821fadf04aaaa39e
Kavindu Dilshan <100995960+ForzenFire@users.noreply.github.com>
2024-12-28 10:37:47 +0530

    Authentication and Authorization

--- src/controller/routes/conductor.js
+++ src/controller/routes/conductor.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { createConductor, getConductors, getConductorById, updateConductor, deleteConductor, } = require('../api/conductorController');
+const { authenticate, authorize } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', createConductor);
-router.get('/',getConductors);
-router.get('/:id', getConductorById);
-router.get('/:id', updateConductor);
-router.get('/:id', deleteConductor);
+router.post('/', authenticate, authorize('admin'), createConductor);
+router.get('/', authenticate, getConductors);
+router.get('/:id', authenticate, getConductorById);
+router.get('/:id', authenticate, authorize('admin'), updateConductor);
+router.get('/:id', authenticate, authorize('admin'), deleteConductor);
 
 module.exports = router;
